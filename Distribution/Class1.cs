d7918c206d387f8976ac29c6d3ea0dafd712b7e1
Тимур Блимготов <timur.blimgotov@mail.ru>
2022-08-15 12:58:06 +0300

    First Commit

---
+++ Distribution/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+namespace Distribution
+{
+    internal class Class1
+    {
+        static void Main()
+        {
+            string DistType = "ПОСЛ";
+            double Sum = 10000;
+            string ArraySum = "1000;2000;3000;5000;8000;5000";
+            
+            Console.Write(Program.Distribute(DistType, Sum, ArraySum));
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Distribution/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Linq;
+
+namespace Distribution
+{
+    public class Program
+    {
+        public static string Distribute(string Method, double sum, string arraySum)
+        {
+            
+            string[] ArrayOfSums = arraySum.Split(';'); //разделяем строку по разделителю ";"
+
+            double[] ArrayOfSumsInitial = new double[ArrayOfSums.Length];//Создаем исходный массив сумм
+                for(int i = 0; i <ArrayOfSums.Length; i++)
+                {
+                    ArrayOfSumsInitial[i] = double.Parse(ArrayOfSums[i]);//Заполняем исходный массив
+                }
+
+            double[] ArrayOfSumsDist = new double[ArrayOfSumsInitial.Length]; //Инициализируем новый массив для выполнения распределений
+
+            switch(Method)
+            {
+                case"ПРОП": //Пропорциональное распределение
+                    double SumAllElements = ArrayOfSumsInitial.Sum();//Определяем сумму всех элементов массива сумм.
+                    //Присваивание массиву пропорционально распределенных значений
+                    ArrayOfSumsDist = ArrayOfSumsInitial.Select(a => (SumAllElements == 0) ? Math.Round(sum / ArrayOfSumsInitial.Length, 2) : Math.Round(value: (double)(sum * a / SumAllElements), 2)).ToArray();
+                    //Присвоение остата последнему элементу массива.
+                    ArrayOfSumsDist[ArrayOfSumsInitial.Length - 1] = Math.Round(ArrayOfSumsDist[ArrayOfSumsInitial.Length - 1] + sum - ArrayOfSumsDist.Sum(), 2);
+                    break;
+
+                case "ПЕРВ": //Реализация распределения в счёт первых элементов массива.
+                    for (int i = 0; i < ArrayOfSumsInitial.Length; i++)
+                    {
+                        if (sum > ArrayOfSumsInitial[i])
+                        {
+                            ArrayOfSumsDist[i] = Math.Round(ArrayOfSumsInitial[i], 2);
+                            sum -= ArrayOfSumsInitial[i];
+                        }
+                        else
+                        {
+                            ArrayOfSumsDist[i] = Math.Round(sum, 2);
+                            sum -= sum;
+                        }
+                    }
+                    break;
+
+                case "ПОСЛ": //Реализация распределения в счёт последних элементов массива.
+                    for (int i = ArrayOfSumsInitial.Length - 1; i >= 0; i--)
+                    {
+                        if (sum > ArrayOfSumsInitial[i])
+                        {
+                            ArrayOfSumsDist[i] = Math.Round(ArrayOfSumsInitial[i], 2);
+                            sum -= ArrayOfSumsInitial[i];
+                        }
+                        else
+                        {
+                            ArrayOfSumsDist[i] = Math.Round(sum, 2);
+                            sum -= sum;
+                        }
+                    }
+                    break;
+        
+
+            }
+
+            return string.Join(";", ArrayOfSumsDist); //Преобразуем массив в строку.
+           
+
+
+        }
+    }
+}
